8c16b9ec1185270652c0afcc932fb26d78cb4318
iamsidar07 <ms8460149@gmail.com>
2024-04-22 21:14:25 +0530

    remove metadata config

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 const config = {
   domain: process.env.DOMAIN,
   mongodbUri: process.env.MONGODB_URI,
-  cloudinaryApiKey: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-  cloudinarySecretKey: process.env.NEXT_PUBLIC_CLOUDINARY_SECRET_KEY,
-  cloudinaryCloudName: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+  cloudinarySecretKey: process.env.CLOUDINARY_SECRET_KEY,
+  cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
 };
 
 export default config;
