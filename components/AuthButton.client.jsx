ceb81dc48bbda3adcfafd255732f16f682f525f1
Manoj Kumar <ms8460149@gmail.com>
2024-05-30 21:53:21 +0530

    import buttonvariants

--- components/AuthButton.client.jsx
+++ components/AuthButton.client.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import UserButton from "./UserButton";
+import { buttonVariants } from "./ui/button";
 
 const AuthButton = ({ session }) => {
   return (
