fea73b46d03648757fdad08e0a3b3265692b8f3c
Manoj Kumar <ms8460149@gmail.com>
2024-05-31 20:08:00 +0530

    fix metatag endpoint

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const config = {
   domain: process.env.NEXT_PUBLIC_DOMAIN,
   mongodbUri: process.env.DATABASE_URL,
   productHuntBadge: process.env.PRODUCT_HUNT_BADGE,
-  dubMetatagEndpointUrl: process.env.LOCATION_ENDPOINT,
+  dubMetatagEndpointUrl: process.env.DUB_METATAG_ENDPOINT,
   locationEndpoint: process.env.LOCATION_ENDPOINT,
   googleClientSecret: process.env.GOOGLE_SECRET,
   googleClientId: process.env.GOOGLE_ID,
