6911e2453c604296bf89ed3376cbea087d7185ac
Manoj Kumar <ms8460149@gmail.com>
2024-05-31 11:52:49 +0530

    move to env

--- config/index.js
+++ config/index.js
@@ -1,12 +1,13 @@
 const config = {
   domain: process.env.NEXT_PUBLIC_DOMAIN,
   mongodbUri: process.env.DATABASE_URL,
-  productHuntBadge:
-    "https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=453724&theme=dark",
-  dubMetatagEndpointUrl: "https://app.dub.co/api/metatags",
-  locationEndpoint: "https://ipapi.co/json/",
+  productHuntBadge: process.env.PRODUCT_HUNT_BADGE,
+  dubMetatagEndpointUrl: process.env.LOCATION_ENDPOINT,
+  locationEndpoint: process.env.LOCATION_ENDPOINT,
   googleClientSecret: process.env.GOOGLE_SECRET,
   googleClientId: process.env.GOOGLE_ID,
+  githubClientSecret: process.env.GITHUB_SECRET,
+  githubClientId: process.env.GITHUB_ID,
 };
 
 export default config;
