7b9498e6c7641818c4ba1ad4cca9943d5f21e3cd
Manoj Kumar <ms8460149@gmail.com>
2024-06-01 11:06:59 +0530

    allow host to everything

--- next.config.mjs
+++ next.config.mjs
@@ -3,27 +3,7 @@ const nextConfig = {
   //reactStrictMode: false,
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/*/image/upload/**",
-      },
-      {
-        protocol: "https",
-        hostname: "*",
-        port: "",
-        pathname: "/favicon.ico",
-      },
-      {
-        hostname: "api.producthunt.com",
-      },
-      {
-        hostname: "opengraph.githubassets.com",
-      },
-      {
-        hostname: "www.google.com",
-      },
+      { hostname: "*" },
     ],
   },
 };
